0e63b6fdc910aff2c2cb0bf43cb6521d3013cd5e
masterBelo <fajjo20@gmail.com>
2018-01-18 18:01:56 -0600

    codigo para notificaciones

--- controllers/requests.php
+++ controllers/requests.php
@@ -31,22 +31,22 @@ class requests extends Common {
 		// turn -> Customer turn
 		// ubication -> Local ubication
 		// zone -> Local zone
-		
+
 	function save($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
 		$rep['status'] = 1;
-		
+
 	// Save customer
 		$rep['result'] = $this -> requestsModel -> save($objet);
-		
+
 		session_start();
 		$_SESSION['dependencie'] = $_POST;
-		
+
 		echo json_encode($rep);
 	}
-	
+
 ///////////////// ******** ----						END save								------ ************ //////////////////
 
 ///////////////// ******** ----						update									------ ************ //////////////////
@@ -72,40 +72,40 @@ class requests extends Common {
 		// turn -> Customer turn
 		// ubication -> Local ubication
 		// zone -> Local zone
-		
-		
+
+
 	function update($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
 		$rep['status'] = 1;
-		
+
 	// Save customer
 		$rep['result'] = $this -> requestsModel -> update($objet);
-		
+
 		echo json_encode($rep);
 	}
-	
+
 ///////////////// ******** ----							END update						------ ************ //////////////////
 
 ///////////////// ******** ----							 add							------ ************ //////////////////
 //////// Loaded the view to add a customer
 	// The parameters that can receive are:
 		// div -> Div where the content is loaded
-	
+
 	function add($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
-		
+
 		if($objet['edit'] == 1){
 			$customer = $this -> requestsModel -> list_requests($objet);
 			$customer = $customer['rows'][0];
 		}
-		
+
 		require ('views/requests/add.php');
 	}
-	
+
 ///////////////// ******** ----						END add								------ ************ //////////////////
 
 ///////////////// ******** ----						list_requests						------ ************ //////////////////
@@ -115,19 +115,19 @@ class requests extends Common {
 		// state -> Dependencie state
 		// municipality -> Dependencie municipality
 		// status -> Requests status
-	
+
 	function list_requests($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
 		session_start();
 		$objet = (empty($objet)) ? $_POST : $objet;
-		
+
 		$requests = $this -> requestsModel -> list_requests($objet);
 		$requests = $requests['rows'];
-		
+
 		foreach ($requests as $key => $value) {
 			$url = ($objet['from_user'] == 1) ? '' : '../';
-			
+
 			$requests[$key]['identificacion'] = str_replace('../../', $url, $value['identificacion']);
 			$requests[$key]['comprobante'] = str_replace('../../', $url, $value['comprobante']);
 			$requests[$key]['fotografia1'] = str_replace('../../', $url, $value['fotografia1']);
@@ -138,20 +138,20 @@ class requests extends Common {
 			$requests[$key]['cartaaceptacion'] = str_replace('../../', $url, $value['cartaaceptacion']);
 			$requests[$key]['sanidad'] = str_replace('../../', $url, $value['sanidad']);
 		}
-		
+
 		$objet_transfer['user_id'] = $_SESSION['user']['id'];
 		$transfer_rights = $this -> requestsModel -> list_transfer_rights($objet_transfer);
 		$transfer_rights = $transfer_rights['rows'];
-		
+
 		if ($objet['json'] == 1) {
 			echo json_encode($requests);
 		} else {
 			$view = (!empty($objet['view'])) ? $objet['view'] : 'list_requests';
-			
+
 			require ('views/requests/'.$view.'.php');
 		}
 	}
-	
+
 ///////////////// ******** ----						END list_requests					------ ************ //////////////////
 
 ///////////////// ******** ----						update_authorize					------ ************ //////////////////
@@ -160,7 +160,7 @@ class requests extends Common {
 		// request_id -> Request ID
 		// status -> 1-> Approved, 2-> Denied
 		// coment -> Request coment
-		
+
 	function update_authorize($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
@@ -168,18 +168,28 @@ class requests extends Common {
 		$rep['status'] = 1;
 		$data['id'] = $objet['request_id'];
 		$data['columns'] = '';
-		
+		$data2['colums1'] = '';
+				echo json_encode($objet);
+
 	// Buil the SQL
 		$data['columns'] .= ($objet['status']) ? ' status = '.$objet['status'].', ' : '' ;
 		$data['columns'] .= ($objet['coment']) ? ' comentario = \''.$objet['coment'].'\', ' : '' ;
 		$data['columns'] = substr($data['columns'], 0, -2);
-		
+
+		$data2['columns1'] .= ($objet['status']) ? ' status = '.$objet['status'].', ' : '' ;
+		$data2['columns1'] .= ($objet['user_id']) ? ' user_id = \''.$objet['user_id'].'\', ' : '' ;
+		$data2['columns1'] .= ($objet['estadomx']) ? ' estadomx = \''.$objet['estadomx'].'\', ' : '' ;
+		$data2['columns1'] .= ($objet['municipiomx']) ? ' municipiomx = \''.$objet['municipiomx'].'\', ' : '' ;
+		$data2['columns1'] .= ($objet['request_id']) ? ' request_id = \''.$objet['request_id'].'\', ' : '' ;
+		$data2['columns1'] = substr($data2['columns1'], 0, -2);
+
 	// Update request information
 		$rep['result'] = $this -> requestsModel -> update($data);
-		
-		echo json_encode($rep);
+		$rep['result'] = $this -> requestsModel -> notificacion($objet);
+
+					echo json_encode($rep);
 	}
-	
+
 ///////////////// ******** ----						END update_authorize				------ ************ //////////////////
 
 ///////////////// ******** ----						load_format							------ ************ //////////////////
@@ -187,22 +197,22 @@ class requests extends Common {
 	// The parameters that can receive are:
 		// request_id -> Request ID
 		// div -> Div where the content is loaded
-	
+
 	function load_format($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
-		
+
 		// require ('formatostianguis/solicituddepermiso.php');
-		
+
 		require('plugins/phpToPDF/phpToPDF.php');
 
-	    //It is possible to include a file that outputs html and store it in a variable 
+	    //It is possible to include a file that outputs html and store it in a variable
 	    //using output buffering.
 	    ob_start();
 	    include('formatostianguis/solicituddepermiso.php');
 	    $my_html = ob_get_clean();
-	
+
 	    //Set Your Options -- we are saving the PDF as 'my_filename.pdf' to a 'my_pdfs' folder
 	    $pdf_options = array(
 	      "source_type" => 'html',
@@ -210,11 +220,11 @@ class requests extends Common {
 	      "action" => 'save',
 	      "save_directory" => 'my_pdfs',
 	      "file_name" => 'my_filename.pdf');
-	
+
 	    //Code to generate PDF file from options above
 	    phptopdf($pdf_options);
 	}
-	
+
 ///////////////// ******** ----						END load_format					------ ************ //////////////////
 
 ///////////////// ******** ----						load_messages					------ ************ //////////////////
@@ -223,18 +233,18 @@ class requests extends Common {
 		// div -> Div where the content is loaded
 		// from -> Origen message(user mail or estate and municipality dependencie)
 		// to -> Destination message(user mail or estate and municipality dependencie)
-	
+
 	function load_messages($objet) {
 	// If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
 	// If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
-		
+
 		$messages = $this -> requestsModel -> list_messages($objet);
 		$messages = $messages['rows'];
-		
+
 		require ('views/requests/list_messages.php');
 	}
-	
+
 ///////////////// ******** ----					END load_messages					------ ************ //////////////////
 
 ///////////////// ******** ----					send_message						------ ************ //////////////////
@@ -245,13 +255,13 @@ class requests extends Common {
 		// request_id -> Request ID
 		// message -> Message to send
 		// div -> Div where the message is loaded
-	
+
 	function send_message($objet) {
     // If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
     // If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
 		$resp['status'] = 1;
-	
+
 	// Validate that the message no are empty
 		$resp['message'] = trim($objet['message']);
 		if (empty($resp['message'])) {
@@ -259,14 +269,14 @@ class requests extends Common {
 			echo json_encode($resp);
 			return;
 		}
-		
+
 	// Save the message
 		$resp['result'] = $this -> requestsModel -> save_message($objet);
 		$resp['message'] = $objet['message'];
-		
+
 		echo json_encode($resp);
 	}
-	
+
 ///////////////// ******** ---- 					FIN send_message					------ ************ //////////////////
 
 ///////////////// ******** ----						transfer_rights						------ ************ //////////////////
@@ -276,55 +286,55 @@ class requests extends Common {
 		// mail -> New user mail
 		// reason -> Reason to transfer
 		// cost -> Cost of the dependencie
-	
+
 	function transfer_rights($objet) {
     // If the object is empty (called from the ajax) it assigns $ _POST that is sent from the index
     // If not, take its normal value
 		$objet = (empty($objet)) ? $_POST : $objet;
 		$resp['status'] = 1;
 		$resp['message'] = 'Todo cool';
-		
+
 		session_start();
 		if($objet['mail'] == $_SESSION['user']['correo']){
 			$resp['status'] = 2;
 			$resp['message'] = 'No puedes ceder los derechos a ti mismo';
-			
+
 			echo json_encode($resp);
 			return;
 		}
-		
+
 	// Validate if the new user exists
 		$exists = $this -> requestsModel -> list_users($objet);
-		
+
 		if($exists['total'] < 1){
 			$resp['status'] = 2;
 			$resp['message'] = 'No existe ningun usuario con el correo actual';
-			
+
 			echo json_encode($resp);
 			return;
 		}
-		
+
 	// Validate if the request exists
 		$objet_transfer['status'] = '0 OR t.status = 1';
 		$objet_transfer['request_id'] = $objet['request_id'];
 		$transfer_rights = $this -> requestsModel -> list_transfer_rights($objet_transfer);
-		
+
 		if($transfer_rights['total'] > 0){
 			$resp['status'] = 2;
 			$resp['message'] = 'Ya existe una solicitud similar a la actual';
-			
+
 			echo json_encode($resp);
 			return;
 		}
-		
+
 	// Save the data
 		$objet['user_id'] =  $_SESSION['user']['id'];
 		$objet['new_user_id'] =  $exists['rows'][0]['id'];
 		$resp['result'] = $this -> requestsModel -> save_transfer_rights($objet);
-		
+
 		echo json_encode($resp);
 	}
-	
+
 ///////////////// ******** ---- 					FIN transfer_rights					------ ************ //////////////////
 
 }
--- models/requests.php
+++ models/requests.php
@@ -27,23 +27,23 @@ class requestsModel extends Connection {
 		// turn -> Customer turn
 		// ubication -> Local ubication
 		// zone -> Local zone
-	
+
 	function save($objet) {
 		$pass = md5($objet['passdep']);
-		
-		$sql = "INSERT INTO 
-						requests(nombredep, direcciondep, numerodep, coloniadep, municipiodep, estadodep, cpdep, teldep, 
+
+		$sql = "INSERT INTO
+						requests(nombredep, direcciondep, numerodep, coloniadep, municipiodep, estadodep, cpdep, teldep,
 									maildep, passdep, inspdep, autdep, mtdep)
-				VALUES	
-					('".$objet['nombredep']."', '".$objet['direcciondep']."', '".$objet['numerodep']."',  
-					'".$objet['coloniadep']."', '".$objet['municipiodep']."', '".$objet['estadodep']."', '".$objet['cpdep']."', 
-					'".$objet['teldep']."', '".$objet['maildep']."', '".$pass."', '".$objet['inspdep']."', 
+				VALUES
+					('".$objet['nombredep']."', '".$objet['direcciondep']."', '".$objet['numerodep']."',
+					'".$objet['coloniadep']."', '".$objet['municipiodep']."', '".$objet['estadodep']."', '".$objet['cpdep']."',
+					'".$objet['teldep']."', '".$objet['maildep']."', '".$pass."', '".$objet['inspdep']."',
 					'".$objet['autdep']."', '".$objet['mtdep']."')";
 		$result = $this -> insert_id($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ----						END save							------ ************ //////////////////
 
 ///////////////// ******** ----						list_requests						------ ************ //////////////////
@@ -53,10 +53,10 @@ class requestsModel extends Connection {
 		// state -> Dependencie state
 		// municipality -> Dependencie municipality
 		// status -> Requests status
-	
+
 	function list_requests($objet) {
 	// Word or ID to search
-		$condition .= (!empty($objet['search'])) ? 
+		$condition .= (!empty($objet['search'])) ?
 			' AND (r.id LIKE \'%'.$objet['search'].'%\' OR r.nombre LIKE \'%'.$objet['search'].'%\')' : '' ;
 	// Filter by the ID if exists
 		$condition .= (!empty($objet['id'])) ? ' AND r.id = '.$objet['id'] : '' ;
@@ -70,7 +70,7 @@ class requestsModel extends Connection {
 		$condition .= (!empty($objet['estado'])) ? ' AND r.estado = \''.$objet['estado'].'\'' : '' ;
 	// Filter by municipality if exists
 		$condition .= (!empty($objet['municipality'])) ? ' AND r.municipiomx = \''.$objet['municipality'].'\'' : '' ;
-		
+
 		$sql = "SELECT
 					r.*, u.id AS user_id, u.name AS user_name, u.curp, d.cost_transfer_rights
 				FROM
@@ -88,10 +88,10 @@ class requestsModel extends Connection {
 				$condition;
 		// return $sql;
 		$result = $this -> query_array($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ----					END list_requests					------ ************ //////////////////
 
 ///////////////// ******** ----						update							------ ************ //////////////////
@@ -100,46 +100,70 @@ class requestsModel extends Connection {
 		// request_id -> Request ID
 		// status -> 1-> Approved, 2-> Denied
 		// coment -> Request coment
-	
+
 	function update($objet) {
-		$sql = "UPDATE 
+		$sql = "UPDATE
 					registros
 				SET ".
-					$objet['columns']." 
+					$objet['columns']."
 				WHERE
 					id = ".$objet['id'];
 		// return $sql;
 		$result = $this -> query($sql);
-		
+
+
+
 		return $result;
 	}
-	
-///////////////// ******** ----						END update							------ ************ //////////////////
 
+///////////////// ******** ----						END update							------ ************ //////////////////
+function notificacion($objet){
+	$notify_date = date("Y-m-d H:i:s");
+
+	if ($objet['status'] == 1) {
+		$sql1 = "INSERT INTO notifications
+							(user_id, fecha_hora, mensaje, estadomx, municipiomx, status, request_id)
+							VALUES ('".$objet['user_id']."', '".$notify_date."', 'Solicitud Aceptada',
+											'".$objet['estadomx']."', '".$objet['municipiomx']."', '".$objet['status']."',
+											'".$objet['request_id']."' )";
+		$result = $this -> query($sql1);
+	}
+	elseif ($objet['status'] == 2) {
+		$sql2 = "INSERT INTO notifications
+							(user_id, fecha_hora, mensaje, estadomx, municipiomx, status, request_id)
+							VALUES ('".$objet['user_id']."', '".$notify_date."', 'Solicitud Rechazada',
+											'".$objet['estadomx']."', '".$objet['municipiomx']."', '".$objet['status']."',
+											'".$objet['request_id']."' )";
+		$result = $this -> query($sql2);
+	}
+
+	return $result;
+
+}
 ///////////////// ******** ---- 					list_messages						------ ************ //////////////////
 //////// Check the messages in the DB and return into array
 	// The parameters can receive:
 		// reservation_id -> Ad ID
 		// user_from -> User that send the message
 		// user_to -> User that recive the message
-	
+
 	function list_messages($objet) {
 	// Select columns(default all *)
 		$select .= (!empty($objet['select'])) ? $objet['select'] : '*';
-		
-		
+
+
 	// Filter by the reservation ID
 		$condition .= (!empty($objet['request_id'])) ? ' AND request_id = '.$objet['request_id'] : '';
 	// Filter by the unread messages and user from
-		$condition .= (!empty($objet['user_unread'])) ? 
+		$condition .= (!empty($objet['user_unread'])) ?
 			' AND (user_to = '.$objet['user_unread'].' && status = 1)' : '';
-		
-		
+
+
 	// Order the results
 		$condition .= (!empty($objet['order'])) ? ' ORDER BY = '.$objet['order'] : ' ORDER BY date';
-		
+
 		$sql = "SELECT ".
-					$select." 					
+					$select."
 				FROM
 					messages
 				WHERE
@@ -147,12 +171,12 @@ class requestsModel extends Connection {
 				$condition;
 		// return $sql;
 		$result = $this -> query_array($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ---- 					END list_messages					------ ************ //////////////////
-	
+
 ///////////////// ******** ---- 					save_message						------ ************ //////////////////
 //////// Save the message in the DB
 	// The parameters can receive:
@@ -160,33 +184,33 @@ class requestsModel extends Connection {
 		// message -> String with the message
 		// from -> User that send the message
 		// to -> User that recive the message
-	
+
 	function save_message($objet) {
 		$date = date("Y-m-d H:i:s");
-		
-		$sql = "INSERT INTO 
+
+		$sql = "INSERT INTO
 					messages(request_id, user_from, user_to, message, date)
 				VALUES
-					('".$objet['request_id']."', '".$objet['from']."', 
+					('".$objet['request_id']."', '".$objet['from']."',
 					'".$objet['to']."', '".$objet['message']."', '".$date."')";
 		$result = $this -> query($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ---- 					END save_message					------ ************ //////////////////
-	
+
 ///////////////// ******** ---- 					list_users							------ ************ //////////////////
 //////// Check the users in the DB and return into array
 	// The parameters can receive:
 		// mail -> User mail
-	
+
 	function list_users($objet) {
 	// Filter by the User mail
 		$condition .= (!empty($objet['mail'])) ? ' AND mail = \''.$objet['mail'].'\'' : '';
-		
-		$sql = "SELECT 
-					* 					
+
+		$sql = "SELECT
+					*
 				FROM
 					users
 				WHERE
@@ -194,17 +218,17 @@ class requestsModel extends Connection {
 				$condition;
 		// return $sql;
 		$result = $this -> query_array($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ---- 					END list_users						------ ************ //////////////////
-	
+
 ///////////////// ******** ---- 					list_transfer_rights				------ ************ //////////////////
 //////// Check the users in the DB and return into array
 	// The parameters can receive:
 		// mail -> User mail
-	
+
 	function list_transfer_rights($objet) {
 	// Filter by the request ID
 		$condition .= (!empty($objet['request_id'])) ? ' AND t.request_id = '.$objet['request_id'] : '';
@@ -212,15 +236,15 @@ class requestsModel extends Connection {
 		$condition .= (!empty($objet['status'])) ? ' AND t.status = '.$objet['status'] : '';
 	// Filter by the status
 		$condition .= (!empty($objet['user_id'])) ? ' AND t.user_id = '.$objet['user_id'] : '';
-		
-		$sql = "SELECT 
-					t.*, u.name AS user_name, 
+
+		$sql = "SELECT
+					t.*, u.name AS user_name,
 					(	SELECT
 							name
 						FROM
 							users
 						WHERE
-							id = t.new_user_id) AS new_user_name 					
+							id = t.new_user_id) AS new_user_name
 				FROM
 					transfers t
 				LEFT JOIN
@@ -232,12 +256,12 @@ class requestsModel extends Connection {
 				$condition;
 		// return $sql;
 		$result = $this -> query_array($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ---- 					END list_transfer_rights			------ ************ //////////////////
-	
+
 ///////////////// ******** ---- 					save_transfer_rights				------ ************ //////////////////
 //////// Save the transfer in the DB
 	// The parameters can receive:
@@ -248,22 +272,22 @@ class requestsModel extends Connection {
 		// cost -> Cost of the dependencie
 		// assignee -> Base 64 string with the frim of the assignee
 		// transferor -> Base 64 string with the frim of the transferor
-	
+
 	function save_transfer_rights($objet) {
 		$date = date("Y-m-d H:i:s");
-		
-		$sql = "INSERT INTO 
+
+		$sql = "INSERT INTO
 					transfers(request_id, user_id, new_user_id, reason, cost, assignee, transferor, date)
 				VALUES
-					('".$objet['request_id']."', '".$objet['user_id']."', '".$objet['new_user_id']."', '".$objet['reason']."', 
+					('".$objet['request_id']."', '".$objet['user_id']."', '".$objet['new_user_id']."', '".$objet['reason']."',
 					 '".$objet['cost']."', '".$objet['assignee']."', '".$objet['transferor']."','".$date."')";
 		$result = $this -> query($sql);
-		
+
 		return $result;
 	}
-	
+
 ///////////////// ******** ---- 				END save_transfer_rights				------ ************ //////////////////
-	
+
 }
 
 ?>
